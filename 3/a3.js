18d8285cbf1ac328c8b1a24dbe8ca7f4dadf9253
Richie <rhuang76@wisc.edu>
2020-10-05 17:20:24 -0500

    added function to draw symmatricallyw

--- 3/a3.js
+++ 3/a3.js
@@ -6,21 +6,39 @@ function setup() {
     
     let start;      // mark the start of time
 
-    // function transform(t) { ctx.setTransform(t[0], t[1], t[3], t[4], t[6], t[7]); }
-    
-
     function draw(timestamp) {
 
+        canvas.width = canvas.width;
+        
+        // calculate time elapsed for animation
+        if (start === undefined) start = timestamp;
+        const elapsed = timestamp - start;
+
+        // array as stack for save and restore emulation
+        var stack = [mat3.create()];
+
+        // config values
+        let head_level = 200;         // from original canvas
+        let scary_ball_level = 250;
+        let ground_level;
+
+        // pseudo canvas save
+        function save() { stack.unshift(mat3.clone(stack[0])); }
+
+        // pseudo canvas restore
+        function restore() { stack.shift(); }
+
+        // mat3.mutiply wrapper
         function mult(T) { return mat3.multiply(stack[0], stack[0], T); }
 
+        // radian getter
         function radian(angle) { return angle*Math.PI/180; } 
 
+        // arc wrapper (start/end slightly differ from original)
         function circle(x, y, radius, start, end) {
             for (let a = start; a < end; ++a)
-                lineTo(x + radius * Math.cos(radian(a)), 
-                        y + radius * Math.sin(radian(a)));
+                lineTo(x+radius*Math.cos(radian(a)), y+radius*Math.sin(radian(a)));
         }
-
         
         // function to move pen in ctx using glmatrix
         function moveTo(x, y) {
@@ -93,54 +111,87 @@ function setup() {
             let leg_size = head_size*4;
 
             // translate to torso
-            stack.unshift(mat3.clone(stack[0]));    // save
+            save();    // save
             let T_to_center = mat3.create();
             mat3.fromTranslation(T_to_center, [0, head_size+torso_size]);
             mult(T_to_center);
 
             // rotate torso for upper body
-            stack.unshift(mat3.clone(stack[0]));    // save
+            save();    // save
             let T_rotate_center = mat3.create();
             mat3.rotate(T_rotate_center, T_rotate_center, radian(10));
             mult(T_rotate_center);
             upper_body(head_size, torso_size);
-            stack.shift();                          // restore
+            restore();                          // restore
 
             lower_body(leg_size);
-            stack.shift();                          // restore
+            restore();                          // restore
         }
 
-        canvas.width = canvas.width;
-        
-        // calculate time elapsed for animation
-        if (start === undefined) start = timestamp;
-        const elapsed = timestamp - start;
+        function scary_ball(color, radius) {
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            circle(0, 0, radius, 270, 360+90);
 
-        // array as stack for save and restore emulation
-        var stack = [mat3.create()];
+            let x, y;
+            for (let a = 90, i = 0; a <= 270; a += 180/6, ++i) {
+                if (i % 2 == 0) x = radius*Math.cos(radian(a));
+                else x = radius*Math.cos(radian(a))-radius*0.4;
+                y = radius*Math.sin(radian(a));
+                lineTo(x, y);
+            }
+            // ctx.stroke();
+            ctx.closePath();
+            ctx.fill();
+        }
 
+        function main_scene(side) {
+            save();         // save for side scaling
+            var T_side = mat3.create();
+            if (side == 'left') {
+                mat3.scale(T_side, T_side, [1, 1]);
+            } else if (side == 'right') {
+                mat3.fromTranslation(T_side, [canvas.width, 0]);
+                mat3.scale(T_side, T_side, [-1, 1])
+            }
+            mult(T_side);
 
-        // drawing left stick man
-        stack.unshift(mat3.clone(stack[0]));
-        var T_to_left_sticker_pos = mat3.create();
-        mat3.fromTranslation(T_to_left_sticker_pos, [100, 200]);
-        mat3.multiply(stack[0], stack[0], T_to_left_sticker_pos);
-        stick_man('#EEE', 5);
-        stack.shift();
-        
-        // drawing right stick man
-        // stack.unshift(mat3.clone(stack[0]));
-        // var T_to_right_sticker_pos = mat3.create();
+            // drawing left stick man
+            save();
+            var T_to_left_sticker_pos = mat3.create();
+            mat3.fromTranslation(T_to_left_sticker_pos, [100, head_level]);
+            mult(T_to_left_sticker_pos);
+            stick_man('#EEE', 5);
+            restore();
+            
+            // drawing scary balls
+            save();
+            var T_to_scary_ball = mat3.create();
+            mat3.fromTranslation(T_to_scary_ball, [400, scary_ball_level]);
+            mat3.scale(T_to_scary_ball, T_to_scary_ball, [1, 0.6]);
+            mult(T_to_scary_ball);
+            scary_ball('#FF0500', 50);
+            scary_ball('#FFA500', 30);
+            restore();
 
+            restore();      // restore from side scaling
+        }
+        // let the drawing begin
+        // some background
 
+        ctx.beginPath();
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = 10;
+        moveTo(0, ground_level);
+        lineTo(canvas.width, ground_level);
+        ctx.stroke();
 
-
-
+        // symmatrical main scenes
+        main_scene('left');
+        main_scene('right');
 
         // TODO: window.requestAnimationFrame(draw);
     }
-
-
 
 
     slider1.addEventListener("input", draw);
