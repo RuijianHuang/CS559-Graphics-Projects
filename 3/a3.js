f6307911d45b6d9c954ecb70c935c7fd69701ad7
Richie <rhuang76@wisc.edu>
2020-10-05 15:02:16 -0500

    completed left stick man static pos; added _generic_ circle drawing func

--- 3/a3.js
+++ 3/a3.js
@@ -6,19 +6,21 @@ function setup() {
     
     let start;      // mark the start of time
 
-    function transform(t) { ctx.setTransform(t[0], t[1], t[3], t[4], t[6], t[7]); }
+    // function transform(t) { ctx.setTransform(t[0], t[1], t[3], t[4], t[6], t[7]); }
     
-    function get_radian(angle) { return angle*Math.PI/180; } 
 
     function draw(timestamp) {
-        canvas.width = canvas.width;
-        
-        // calculate time elapsed for animation
-        if (start === undefined) start = timestamp;
-        const elapsed = timestamp - start;
-        
-        
-        var stack = [mat3.create()];
+
+        function mult(T) { return mat3.multiply(stack[0], stack[0], T); }
+
+        function radian(angle) { return angle*Math.PI/180; } 
+
+        function circle(x, y, radius, start, end) {
+            for (let a = start; a < end; ++a)
+                lineTo(x + radius * Math.cos(radian(a)), 
+                        y + radius * Math.sin(radian(a)));
+        }
+
         
         // function to move pen in ctx using glmatrix
         function moveTo(x, y) {
@@ -53,25 +55,87 @@ function setup() {
             ctx.fill();
         }
 
-        function sticker(color, linewith) {
+        function upper_body(head_size, torso_size) {
             ctx.beginPath();
-            ctx.strokeStyle = color;;
-            ctx.lineWidth = linewith;
+            circle(0, -torso_size-head_size, head_size, 90, 90+360);
+            lineTo(0, 0);
 
-            let radius = 50
-            for (let i = 0; i < 360; ++i) {
-                let r = get_radian(i);
-                lineTo(radius*Math.cos(r), radius*Math.sin(r));
-            }
-            ctx.closePath();
+            let armpit = -torso_size/5*4;
+
+            // left arm
+            moveTo(0, armpit);
+            lineTo(-20, armpit/2);
+            lineTo(-10, armpit/5);
+
+            // right arm
+            moveTo(0, armpit);
+            lineTo(10, armpit/2);
+            lineTo(20, armpit/5);
+
+            ctx.stroke();
+        }
+        
+        function lower_body(leg_size) {
+            ctx.beginPath();
+            moveTo(0, 0);       // pelvis position
+            lineTo(-10, leg_size);
+            moveTo(0, 0);
+            lineTo(10, leg_size);
             ctx.stroke();
         }
 
-        var tx_test = mat3.create();
-        mat3.fromTranslation(tx_test, [400, 100]);
-        mat3.rotate(tx_test, tx_test, 90*Math.PI/180);
-        mat3.multiply(stack[0], stack[0], tx_test);
-        sticker('#FFF', 5);
+        function stick_man(color, linewidth) {
+            ctx.strokeStyle = color;
+            ctx.lineWidth = linewidth;
+            
+            let head_size = 20;
+            let torso_size = head_size*4;
+            let leg_size = head_size*4;
+
+            // translate to torso
+            stack.unshift(mat3.clone(stack[0]));    // save
+            let T_to_center = mat3.create();
+            mat3.fromTranslation(T_to_center, [0, head_size+torso_size]);
+            mult(T_to_center);
+
+            // rotate torso for upper body
+            stack.unshift(mat3.clone(stack[0]));    // save
+            let T_rotate_center = mat3.create();
+            mat3.rotate(T_rotate_center, T_rotate_center, radian(10));
+            mult(T_rotate_center);
+            upper_body(head_size, torso_size);
+            stack.shift();                          // restore
+
+            lower_body(leg_size);
+            stack.shift();                          // restore
+        }
+
+        canvas.width = canvas.width;
+        
+        // calculate time elapsed for animation
+        if (start === undefined) start = timestamp;
+        const elapsed = timestamp - start;
+
+        // array as stack for save and restore emulation
+        var stack = [mat3.create()];
+
+
+        // drawing left stick man
+        stack.unshift(mat3.clone(stack[0]));
+        var T_to_left_sticker_pos = mat3.create();
+        mat3.fromTranslation(T_to_left_sticker_pos, [100, 200]);
+        mat3.multiply(stack[0], stack[0], T_to_left_sticker_pos);
+        stick_man('#EEE', 5);
+        stack.shift();
+        
+        // drawing right stick man
+        // stack.unshift(mat3.clone(stack[0]));
+        // var T_to_right_sticker_pos = mat3.create();
+
+
+
+
+
 
         // TODO: window.requestAnimationFrame(draw);
     }
