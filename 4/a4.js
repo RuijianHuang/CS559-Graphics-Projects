ea27d337789dc15db5407d60d31a22168cc890fd
Richie <rhuang76@wisc.edu>
2020-10-18 23:20:31 -0500

    added spiral and hermite setup; tweaked from demo

--- 4/a4.js
+++ 4/a4.js
@@ -1,3 +1,9 @@
+// helper to insert a point into an array in hermite cubic formatting
+Array.prototype.pushPoint = function(p, d) {
+    let last = this[this.length-1];
+    this.push([last[2], last[3], p, d]);
+}
+
 function setup() {
     let canvas = document.getElementById('myCanvas');
     let ctx = canvas.getContext('2d');
@@ -8,13 +14,6 @@ function setup() {
     let start;                      // mark the start of time
     let elapsed;                    // time elapsed
     let cycle = slider1.value;               // define length of a animation cycle
-
-    // random(num)
-    function jiggle(num, directional) {
-        if (directional) return Math.random()*num;
-        if (Math.random() > 0.5) return Math.random()*num;
-        else return -Math.random()*num;
-    }
 
     // current time elapsed proportional to a cycle
     function get_proportion() { return elapsed%cycle/cycle; }
@@ -63,24 +62,111 @@ function setup() {
         ctx.fill();
     }
 
+	// function Cspiral_tangent(t) {
+	//     var R = Rslope * t + Rstart;
+	//     var Rprime = Rslope;
+	//     var x = Rprime * Math.cos(2.0 * Math.PI * t)
+    //             - R * 2.0 * Math.PI * Math.sin(2.0 * Math.PI * t);
+	//     var y = Rprime * Math.sin(2.0 * Math.PI * t)
+    //             + R * 2.0 * Math.PI * Math.cos(2.0 * Math.PI * t);
+	//     return [x,y];
+	// }
+
+    function curve0(t) {    // normal parabola
+        let x = t;
+        let y = t*t;
+        return [x, y];
+    }
+    
+    function curve1(t) {    // C1 continuity at t=1 to curve0
+        let x = t;
+        let y = -t*t + 4*t -2;
+        return [x, y];
+    }
+    
+	function spiral(t) {
+        let r = 0.3;
+	    var x = t/3 + r*Math.cos(2*Math.PI*t);
+	    var y = r*Math.sin(2*Math.PI*t);
+	    return [x,y];
+	}
+
+    function hermite_basis(t) {
+        return [2*t*t*t-3*t*t+1,
+                t*t*t-2*t*t+t,
+                -2*t*t*t+3*t*t,
+                t*t*t-t*t
+                ];
+    }
+
+    function hermite_cubic(P,t){
+      var b = hermite_basis(t);
+      var result=vec2.create();
+      vec2.scale(result,P[0],b[0]);
+      vec2.scaleAndAdd(result,result,P[1],b[1]);
+      vec2.scaleAndAdd(result,result,P[2],b[2]);
+      vec2.scaleAndAdd(result,result,P[3],b[3]);
+      return result;
+    }
+   
+    function drawCurve(t0, t1, granularity, curve, T, color, P) {
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 10;
+        ctx.beginPath();
+
+        P ? moveTo(curve(P, t0), T) : moveTo(curve(t0), T);     // P == null -> not hermite
+        for(let i = 0; i <= granularity; ++i) {
+            let p = (granularity - i)/granularity;
+            let t = p*t0 + (i/granularity)*t1;
+            let coor = P ? curve(P, t) : curve(t); 
+
+            if (t0 >= 2)
+                console.log(coor[0], coor[1]);
+            lineTo(coor[0], coor[1]);
+        }
+        ctx.stroke();
+    }
+
     function draw(timestamp) {
         canvas.width = canvas.width;
-        cycle = slider1.value;
-        sparkle = 5;
-        
+
         // calculate time elapsed for animation
         if (start === undefined) start = timestamp;
         elapsed = timestamp - start;
 
+        let T_to_test = mat3.create();
+        mat3.fromTranslation(T_to_test, [100, 600]);
+        mat3.scale(T_to_test, T_to_test, [100, -100]);
+        save();
+        mult(T_to_test);
 
+        // drawCurve(0, 1, 200, curve0, T_to_test, "tan", null);
+        // drawCurve(1, 4, 200, curve1, T_to_test, "green", null);
+        // drawCurve(2, 4, 200, spiral, T_to_test, "brown", null);
 
+        for(let i = 0; i < pls.length; ++i) {
+            drawCurve(0, 1, 200, hermite_cubic, T_to_test, "#bbb", pls[i]);
+        }
+        restore();
+        // window.requestAnimationFrame(draw); TODO
+    }
 
-        // window.requestAnimationFrame(draw);  FIXME: animation recursion
+    
+    let pls =   [[[0, 0],   [2, 0], 
+                  [1, 1],   [1, 3]]];
+    pls.pushPoint([2, 2.5], [1, 0]);
+    pls.pushPoint([3, 1],   [1, -3]);
+    pls.pushPoint([4, 0],   [1.8, 0]);
+    for(let i = 0; i < 3; ++i) {
+        let j = i + 4;
+        pls.pushPoint([j+1, 1.5],   [0, 1.5]);
+        pls.pushPoint([j+0.5, 3],   [-1, 0]);
+        pls.pushPoint([j, 1.5],     [0, -1.5]);
+        pls.pushPoint([j+1, 0],     [1.8, 0]);
     }
 
     slider1.addEventListener("input", draw);
-
-    draw(100);  //FIXME: animation perhaps?
-    // window.requestAnimationFrame(draw);
+    draw(100);
+    // window.requestAnimationFrame(draw);  TODO
 }
 window.onload=setup();
